3c6ff6ec9a99cbd0a22f1d73440ebf38cd7d2b8b
Willy D <wilsonr.delgadov@uqvirtual.edu.co>
2024-02-10 14:31:55 -0500

    modify clase controlador

--- taller1Almacen/src/main/java/co/edu/uniquindio/programacion3/taller1almacen/HelloController.java
+++ taller1Almacen/src/main/java/co/edu/uniquindio/programacion3/taller1almacen/HelloController.java
@@ -11,17 +11,34 @@ public class HelloController {
 
     @FXML
     private Label labePrueba;
+    @FXML
     private Label labelDatosPrueba;
+
+    //Capturar los datos del cliente:
     @FXML
     private TextField nombreCliente;
+    @FXML
+    private TextField apellidoCliente;
+    @FXML
+    private TextField idCliente;
+    @FXML
+    private TextField direccionCliente;
+    @FXML
+    private TextField telefonoCliente;
 
 
     @FXML
     protected void hacerClick() {
-        labePrueba.setText("Funciona!");
+        //obtener los datos del cliente
         String textoNombre = nombreCliente.getText();
+        String textoApellido = apellidoCliente.getText();
+        String textoId = idCliente.getText();
+        String textoDireccion = direccionCliente.getText();
+        String textoTelefono = telefonoCliente.getText();
+        //String texto = Cliente.getText();
        // labelDatosPrueba.setText(textoNombre);
         labePrueba.setText(textoNombre);
+        labelDatosPrueba.setText("bienvenido: "+textoNombre+"\n"+"");
 
     }
 
