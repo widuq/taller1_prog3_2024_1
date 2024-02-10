bd0563ec83a504054100142c4cc4807cae7d197b
Willy D <wilsonr.delgadov@uqvirtual.edu.co>
2024-02-09 19:34:14 -0500

    add interfaz grafica

--- taller1Almacen/src/main/java/co/edu/uniquindio/programacion3/taller1almacen/HelloController.java
+++ taller1Almacen/src/main/java/co/edu/uniquindio/programacion3/taller1almacen/HelloController.java
@@ -2,10 +2,29 @@ package co.edu.uniquindio.programacion3.taller1almacen;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
 
 public class HelloController {
     @FXML
     private Label welcomeText;
+
+    @FXML
+    private Label labePrueba;
+    private Label labelDatosPrueba;
+    @FXML
+    private TextField nombreCliente;
+
+
+    @FXML
+    protected void hacerClick() {
+        labePrueba.setText("Funciona!");
+        String textoNombre = nombreCliente.getText();
+       // labelDatosPrueba.setText(textoNombre);
+        labePrueba.setText(textoNombre);
+
+    }
+
 
     @FXML
     protected void onHelloButtonClick() {
