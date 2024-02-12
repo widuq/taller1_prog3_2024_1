d7e45ceea817c914c4e3af6883be043127ac4024
alejilla222 <sanabria.aleja22@gmail.com>
2024-02-12 14:36:58 -0500

    Agregar tabla

--- taller1Almacen/src/main/java/co/edu/uniquindio/programacion3/taller1almacen/HelloApplication.java
+++ taller1Almacen/src/main/java/co/edu/uniquindio/programacion3/taller1almacen/HelloApplication.java
@@ -11,7 +11,7 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 620, 520);
+        Scene scene = new Scene(fxmlLoader.load(), 1020, 520);
         stage.setTitle("Almacen AWJ v2.01");
         stage.setScene(scene);
         stage.show();
